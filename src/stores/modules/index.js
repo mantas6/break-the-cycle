1f0472316d2a7d6114f00d563f5f727c69f774b7
mantas <mantas.pauliukonis@frontit.dk>
2023-12-09 08:35:40 +0200

    Glob Import actions - eager loading

--- src/stores/modules/index.js
+++ src/stores/modules/index.js
@@ -1,5 +1,5 @@
 export function initializeDynamicModules() {
-    const modules = import.meta.glob('./*/*.js', { eager: true });
+    const modules = import.meta.glob('./**/*.js', { eager: true });
 
     Object.values(modules).forEach(async (mod) => {
         mod.default();
