cd414bf8074b68a1bbd0a448ff7ecf6bfb5aa429
mantas <mantas.pauliukonis@frontit.dk>
2023-12-09 11:20:05 +0200

    Save and load controls

--- src/components/HeaderPanel.vue
+++ src/components/HeaderPanel.vue
@@ -1,23 +1,19 @@
 <script setup>
 import { useTimeStore } from "@/stores/time.js";
 import { RouterLink } from "vue-router";
-import { resetGame } from "@/routines/persistence.js";
+import { resetGame, loadGame, saveGame } from "@/routines/persistence.js";
 
 const time = useTimeStore();
-
-function reset() {
-  resetGame();
-}
 </script>
 
 <template>
   <nav class="w-full flex gap-3 p-3">
     <div>Break the Cycle</div>
-    <span>|</span>
     <div class="w-24 text-center">{{ time.date }}</div>
-    <span>|</span>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
-    <button @click="reset">Reset</button>
+    <button @click="resetGame">Reset</button>
+    <button @click="saveGame">Save</button>
+    <button @click="loadGame">Load</button>
   </nav>
 </template>
