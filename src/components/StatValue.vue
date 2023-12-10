92263f36fca61537005406161b97fe371e2f7bf1
mantas <mantas.pauliukonis@frontit.dk>
2023-12-10 09:34:58 +0200

    StatValue component

---
+++ src/components/StatValue.vue
@@ -0,0 +1,13 @@
+<script setup>
+  const props = defineProps(['title', 'value', 'unit']);
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <div>{{ title }}</div>
+    <div class="flex">
+      <span>{{ value }}</span>
+      <span v-if="unit">{{ unit }}</span>
+    </div>
+  </div>
+</template>
--- src/components/panels/DetailsPanel.vue
+++ src/components/panels/DetailsPanel.vue
@@ -4,6 +4,7 @@ import Panel from "@/components/Panels/Panel.vue";
 import { useWalletStore } from "@/stores/stats/wallet";
 import { useNutritionStore } from "@/stores/stats/nutrition";
 import { usePassportStore } from "@/stores/stats/passport";
+import StatValue from "@/components/StatValue.vue";
 
 const wallet = useWalletStore();
 const nutrition = useNutritionStore();
@@ -12,8 +13,13 @@ const passport = usePassportStore();
 
 <template>
   <Panel>
-    <div>Balance: {{ wallet.balance }}$</div>
-    <div>Age: {{ passport.age }}</div>
-    <div>Calories: {{ nutrition.calories.now }}</div>
+    <div class="grid grid-cols-2 gap-3">
+      <div>
+        <StatValue title="Balance" :value="wallet.balance" unit="$" />
+        <StatValue title="Age" :value="passport.age" />
+        <StatValue title="Calories" :value="nutrition.calories.now" />
+      </div>
+      <div>Test</div>
+    </div>
   </Panel>
 </template>
