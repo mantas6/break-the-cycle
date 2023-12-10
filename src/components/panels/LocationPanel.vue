9fe2cdf2f4877d51052d2da02fbc425bdb99940a
mantas <mantas.pauliukonis@frontit.dk>
2023-12-10 09:39:12 +0200

    Settings view

--- src/components/panels/LocationPanel.vue
+++ src/components/panels/LocationPanel.vue
@@ -1,15 +1,12 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import { loadGame, resetGame, saveGame } from "@/routines/persistence.js";
 import Panel from "@/components/Panels/Panel.vue";
 </script>
 
 <template>
   <Panel class="flex gap-3 items-start">
     <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/settings">Settings</RouterLink>
     <RouterLink to="/about">About</RouterLink>
-    <button @click="resetGame">Reset</button>
-    <button @click="saveGame">Save</button>
-    <button @click="loadGame">Load</button>
   </Panel>
 </template>
---
+++ src/views/SettingsView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import { loadGame, resetGame, saveGame } from "@/routines/persistence";
+</script>
+
+<template>
+  <div class="flex flex-col gap-3">
+    <button @click="saveGame">Save</button>
+    <button @click="loadGame">Load</button>
+    <button @click="resetGame">Reset</button>
+  </div>
+</template>
