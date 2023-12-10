beb1bad391f3f98fd4e6337115d17c5997ebc6cb
mantas <mantas.pauliukonis@frontit.dk>
2023-12-10 09:13:01 +0200

    Refactor layout components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import ProductivityPanel from "@/components/panels/ProductivityPanel.vue";
+import HeaderPanel from "@/components/panels/HeaderPanel.vue";
 import LocationPanel from "@/components/panels/LocationPanel.vue";
 import Panel from "@/components/panels/Panel.vue";
 import PlannerPanel from "@/components/panels/PlannerPanel.vue";
@@ -9,7 +9,7 @@ import DetailsPanel from "@/components/panels/DetailsPanel.vue";
 
 <template>
   <div class="grid grid-cols-4 gap-3 m-3">
-    <ProductivityPanel />
+    <HeaderPanel />
     <LocationPanel class="col-span-2" />
     <DetailsPanel class="row-span-2" />
 
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="flex gap-1">
-    <span>Break</span>
-    <span>the</span>
-    <span>Cycle</span>
+  <div>
+    <div class="flex gap-1">
+      <span>Break</span>
+      <span>the</span>
+      <span>Cycle</span>
+    </div>
+    <div class="text-xs text-zinc-300">Will you break the cycle?</div>
   </div>
 </template>
+<script setup>
+</script>
