81866119e013890eec891deb74de8681aaae1a78
mantas <mantas.pauliukonis@frontit.dk>
2023-12-15 12:01:58 +0200

    Basic physical regen

--- src/stores/stats/physical.js
+++ src/stores/stats/physical.js
@@ -6,7 +6,13 @@ import { Balance } from "@/stats";
 export const usePhysicalStore = defineStore(storeName(import.meta.url), () => {
     const balance = Balance.create(0, -1000, 0, 1000);
 
+    function onClock() {
+        Balance.affect(balance, -3)
+    }
+
     return {
+        onClock,
+
         balance,
     };
 })
