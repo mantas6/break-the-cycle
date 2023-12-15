87d14b820841f71e8f060c898e3242c00b2e693d
mantas <mantas.pauliukonis@frontit.dk>
2023-12-15 10:17:31 +0200

    Improve BalanceStat component

--- src/components/StatBalance.vue
+++ src/components/StatBalance.vue
@@ -3,22 +3,32 @@ import {computed} from "vue";
 
 const props = defineProps(['title', 'now', 'min', 'max', 'center']);
 
-const width = computed(() => 20)
+const left = computed(() => {
+  const minBoundAbs = Math.abs(props.min);
+  const normalized = props.now + minBoundAbs;
+  return Math.round(normalized / (props.max + minBoundAbs) * 100);
+})
 </script>
 
 <template>
   <div class="text-xs flex flex-col gap-1">
     <div class="text-center font-bold">{{ title }}</div>
     <div class="w-full bg-gray-200 overflow-hidden relative h-4">
-      <div class="bg-blue-500 h-4 absolute" :style="{ width: width + '%', left: '50%' }"></div>
+      <div class="absolute w-1/6 h-2 bg-red-300 bottom-0"></div>
+      <div class="absolute w-1/6 h-2 bg-red-300 bottom-0 right-0"></div>
+
+      <div class="absolute w-full flex justify-center bottom-0"><div class="bg-green-300 h-2 w-1/4"></div></div>
+
       <div class="absolute w-full flex justify-center">
-        <div class="bg-red-500 h-4 w-1"></div>
+        <div class="border-dotted border-l border-zinc-600 h-2"></div>
       </div>
+
+      <div class="absolute bg-red-500 h-4 w-1" :style="{ left: `calc(${left}% - 2px)` }"></div>
     </div>
     <div class="flex justify-between">
-      <span>Lazy</span>
+      <span class="border-l pl-1 border-dotted">Lazy</span>
       <span>OK</span>
-      <span>Tired</span>
+      <span class="border-r pr-1 border-dotted">Tired</span>
     </div>
   </div>
 </template>
