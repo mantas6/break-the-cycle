43841c8d04857fd12b46e7ad1f5f0239f44a96c9
mantas <mantas.pauliukonis@frontit.dk>
2023-12-17 06:20:54 +0200

    Clean up

--- src/components/panels/DetailsPanel.vue
+++ src/components/panels/DetailsPanel.vue
@@ -2,17 +2,13 @@
 import Panel from "@/components/Panels/Panel.vue";
 
 import { useWalletStore } from "@/stores/stats/wallet";
-import { useNutritionStore } from "@/stores/stats/nutrition";
 import { usePassportStore } from "@/stores/stats/passport";
-import { useMuscleStore } from "@/stores/stats/muscle";
 import { usePhysicalStore } from "@/stores/stats/physical";
 import StatValue from "@/components/StatValue.vue";
 import StatBalance from "@/components/StatBalance.vue";
 
 const wallet = useWalletStore();
-const nutrition = useNutritionStore();
 const passport = usePassportStore();
-const muscle = useMuscleStore();
 const physical = usePhysicalStore();
 </script>
 
--- src/stores/modules/actions/food/fastFood.js
+++ src/stores/modules/actions/food/fastFood.js
@@ -1,6 +1,5 @@
 import {ref, computed, reactive} from 'vue'
 import { useWalletStore } from "@/stores/stats/wallet";
-import { useNutritionStore } from "@/stores/stats/nutrition";
 import {defineStore} from "pinia";
 import { storeName } from "@/stores";
 
@@ -11,24 +10,7 @@ export default defineStore(storeName(import.meta.url), () => {
     const meta = reactive({})
 
     function executeAction(count) {
-        const wallet = useWalletStore();
-        const nutrition = useNutritionStore();
 
-        const calorieGain = 1 * count;
-
-        const caloriesDemand = nutrition.preModifyCalories(calorieGain);
-
-        const baseCost = 0.5;
-        const totalCost = baseCost * caloriesDemand * -1;
-
-        if (totalCost < 0) {
-            const availableBalance = wallet.preTransaction(totalCost);
-
-            const availablePercent = Math.abs(availableBalance) / Math.abs(totalCost);
-
-            wallet.transaction(availableBalance);
-            nutrition.modifyCalories(caloriesDemand * availablePercent);
-        }
     }
 
     return {
