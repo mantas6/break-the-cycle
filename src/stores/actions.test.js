55f62f19307d879e53f0d900b3b1a097c15639df
mantas <mantas.pauliukonis@frontit.dk>
2023-12-22 09:53:17 +0200

    Use generic action for test

--- src/stores/actions.test.js
+++ src/stores/actions.test.js
@@ -1,41 +1,61 @@
 import { expect, it, beforeEach } from 'vitest'
-import {createPinia, setActivePinia} from "pinia";
+import {createPinia, defineStore, setActivePinia} from "pinia";
 import {useActionsStore} from "@/stores/actions.js";
-import useJanitorStore from "@/stores/modules/actions/jobs/janitor";
 import {actionStores} from "@/plugins/actions.js";
+import {computed, reactive} from "vue";
+import {usePhysicalStore} from "@/stores/stats/physical.js";
+import {Balance} from "@/stats/index.js";
+import {useWalletStore} from "@/stores/stats/wallet.js";
+
+const useTestStore = defineStore('test-action', () => {
+    const title = computed(() => 'store');
+    const durations = computed(() => [4, 8, 12]);
+
+    function executeAction(count) {
+        //
+    }
+
+    return {
+        title,
+        durations,
+
+        executeAction,
+    };
+})
+
 
 beforeEach(() => {
     const store = createPinia();
     setActivePinia(store)
 
-    const janitor = useJanitorStore()
+    const action = useTestStore()
     actionStores.value.clear();
-    actionStores.value.set(janitor.$id, janitor)
+    actionStores.value.set(action.$id, action)
 
 })
 
 it('correctly controls durations', () => {
-    const store = useActionsStore();
-    const janitor = useJanitorStore()
+    const actions = useActionsStore();
+    const store = useTestStore()
 
-    expect(store.currentDuration).toBe(0)
-    expect(store.maxDuration).toBe(24)
+    expect(actions.currentDuration).toBe(0)
+    expect(actions.maxDuration).toBe(24)
 
 
-    expect(store.all).toHaveProperty(janitor.$id)
-    expect(store.allActive).not.toHaveProperty(janitor.$id)
+    expect(actions.all).toHaveProperty(store.$id)
+    expect(actions.allActive).not.toHaveProperty(store.$id)
 
-    store.increase(janitor.$id)
-    expect(store.currentDuration).toBe(4)
-    expect(store.canIncrease(janitor.$id)).toBe(true)
+    actions.increase(store.$id)
+    expect(actions.currentDuration).toBe(4)
+    expect(actions.canIncrease(store.$id)).toBe(true)
 
-    expect(store.allActive).toHaveProperty(janitor.$id)
+    expect(actions.allActive).toHaveProperty(store.$id)
 
-    store.increase(janitor.$id)
-    expect(store.currentDuration).toBe(8)
-    expect(store.canIncrease(janitor.$id)).toBe(true)
+    actions.increase(store.$id)
+    expect(actions.currentDuration).toBe(8)
+    expect(actions.canIncrease(store.$id)).toBe(true)
 
-    store.increase(janitor.$id)
-    expect(store.currentDuration).toBe(12)
-    expect(store.canIncrease(janitor.$id)).toBe(false)
+    actions.increase(store.$id)
+    expect(actions.currentDuration).toBe(12)
+    expect(actions.canIncrease(store.$id)).toBe(false)
 })
