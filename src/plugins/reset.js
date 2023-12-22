99ac7e3fcec3f3b79210e77a3a4fffad438f59a2
mantas <mantas.pauliukonis@frontit.dk>
2023-12-22 08:04:17 +0200

    Add note

--- src/plugins/reset.js
+++ src/plugins/reset.js
@@ -3,8 +3,9 @@ import { cloneDeep } from "lodash/lang";
 export function ResetPlugin({ store }) {
     const initialState = cloneDeep(store.$state);
 
+    // TODO: check if store implements $reset
     store.$reset = () => {
-        // store.$patch($state => Object.assign($state, initialState));
+        //store.$patch($state => Object.assign($state, initialState));
         store.$patch(initialState);
     };
 }
