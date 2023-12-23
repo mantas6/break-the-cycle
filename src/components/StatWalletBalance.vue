f9b47982c00d4734039d833423fb530594f1fcd3
mantas <mantas.pauliukonis@frontit.dk>
2023-12-23 12:10:32 +0200

    Wallet balance component add colors based to diff

--- src/components/StatWalletBalance.vue
+++ src/components/StatWalletBalance.vue
@@ -11,15 +11,15 @@ const props = defineProps({
 
 <template>
   <div class="flex flex-col">
-    <div class="text-3xl text-center"><NumberFormat format="currency" :value="now" /></div>
+    <div class="text-3xl text-center" :class="loss > gain ? 'text-red-200' : 'text-green-200'"><NumberFormat format="currency" :value="now" /></div>
     <div class="flex justify-between text-xs">
       <div class="flex gap-1">
         <ChevronUpIcon class="w-4 text-green-500" />
-        <span><NumberFormat format="currency" :value="gain" /></span>
+        <span :class="{ 'text-zinc-400': gain === 0 }"><NumberFormat format="currency" :value="gain" /></span>
       </div>
       <div class="flex gap-1">
         <ChevronDownIcon class="w-4 text-red-500" />
-        <span><NumberFormat format="currency" :value="loss" /></span>
+        <span :class="{ 'text-zinc-400': loss === 0 }"><NumberFormat format="currency" :value="loss" /></span>
       </div>
     </div>
   </div>
