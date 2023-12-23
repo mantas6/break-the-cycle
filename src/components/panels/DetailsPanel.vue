9c9c90218da36c8cc99b0bc5fb7c4a20040ca00e
mantas <mantas.pauliukonis@frontit.dk>
2023-12-23 12:04:47 +0200

    Wallet balance component - remove old

--- src/components/panels/DetailsPanel.vue
+++ src/components/panels/DetailsPanel.vue
@@ -1,7 +1,6 @@
 <script setup>
 import PanelBlock from "@/components/Panels/PanelBlock.vue";
 
-import { useWalletStore } from "@/stores/stats/wallet";
 import { usePassportStore } from "@/stores/stats/passport";
 import { usePhysicalStore } from "@/stores/stats/physical";
 import { useNutritionStore } from "@/stores/stats/nutrition";
@@ -9,7 +8,6 @@ import StatValue from "@/components/StatValue.vue";
 import StatBalance from "@/components/StatBalance.vue";
 import StatBalanceFill from "@/components/StatBalanceFill.vue";
 
-const wallet = useWalletStore();
 const passport = usePassportStore();
 const physical = usePhysicalStore();
 const nutrition = useNutritionStore();
@@ -20,9 +18,6 @@ const nutrition = useNutritionStore();
     <div class="grid gap-3">
       <div>
         <StatValue title="Age" :now="passport.age" />
-        <StatValue title="Income" format="currency" :value-class="{ 'text-red-300': wallet.balance.gain < wallet.balance.loss }" :now="wallet.balance.gain" />
-        <StatValue title="Outcome" format="currency" :now="wallet.balance.loss" />
-        <StatValue title="Balance" format="currency" :now="wallet.balance.now" />
       </div>
       <div class="flex flex-col gap-3">
         <StatBalance title="Physical" title-min="Tired" title-max="Lazy" v-bind="physical.energy" />
