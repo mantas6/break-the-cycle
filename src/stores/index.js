8d270ce8e9c0e6dbe5a5a4333fc3b57c14e4a310
mantas <mantas.pauliukonis@frontit.dk>
2023-12-23 08:17:19 +0200

    Add store name unique warning

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,13 @@
 import { kebabCase } from "lodash/string";
 
+const storeNames = [];
+
 export function storeName(name) {
+    if (storeNames.includes(name)) {
+        console.warn('Store name is already defined ' + name);
+    } else {
+        storeNames.push(name);
+    }
+
     return kebabCase(name);
 }
