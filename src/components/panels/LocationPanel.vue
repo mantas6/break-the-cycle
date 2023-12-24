4d201fe9496f39f9c9df1182aad58a4799be04fa
mantas <mantas.pauliukonis@frontit.dk>
2023-12-24 11:38:03 +0200

    Action unlock notifications in location panel

--- src/components/panels/LocationPanel.vue
+++ src/components/panels/LocationPanel.vue
@@ -9,11 +9,17 @@ import { RouterLink } from "vue-router";
 import PanelBlock from "@/components/Panels/PanelBlock.vue";
 import { useTimeStore } from "@/stores/time";
 import { useActionsStore } from "@/stores/actions";
+import {pickBy} from "lodash/object";
 
 const time = useTimeStore();
 const actions = useActionsStore();
 
 const categories = computed(() => uniq(map(actions.all, 'category')));
+
+const categoriesNotify = computed(() => {
+  const actionsNotify = pickBy(actions.all, action => action.notify)
+  return uniq(map(actionsNotify, 'category'));
+});
 </script>
 
 <template>
@@ -21,7 +27,10 @@ const categories = computed(() => uniq(map(actions.all, 'category')));
     <div class="flex gap-3">
       <RouterLink to="/" v-hover>Home</RouterLink>
       <template v-for="category in categories" :key="category">
-        <RouterLink :to="`/${kebabCase(category)}`" v-hover>{{ category }}</RouterLink>
+        <RouterLink :to="`/${kebabCase(category)}`" v-hover>
+          <span>{{ category }}</span>
+          <span v-if="categoriesNotify.includes(category)" class="text-red-300">*</span>
+        </RouterLink>
       </template>
     </div>
     <div class="flex gap-2 items-start">
