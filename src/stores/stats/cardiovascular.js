387358e20562236125bffa3837439670ffbb5bd3
mantas <mantas.pauliukonis@frontit.dk>
2023-12-25 13:28:09 +0200

    computedOnce func use

--- src/stores/stats/cardiovascular.js
+++ src/stores/stats/cardiovascular.js
@@ -1,13 +1,13 @@
 import { defineStore } from 'pinia'
 import { storeName } from "@/stores";
 import { Balance } from "@/stats";
-import {computed} from "vue";
+import {computedOnce} from "@/stats/computed.js";
 
 export const useCardiovascular = defineStore(storeName('cardiovascular'), () => {
     const health = Balance.create(0, 1000, 1000);
     const healthLifetime = Balance.create(0, 1000, 1000);
 
-    const overallHealth = computed(() => Balance.percentage(health) * Balance.percentage(healthLifetime));
+    const overallHealth = computedOnce(() => Balance.percentage(health) * Balance.percentage(healthLifetime));
 
     return {
         health,
