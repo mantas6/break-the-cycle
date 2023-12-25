95280d5d77d9a000099d3f39d380185ddb2a3fa8
mantas <mantas.pauliukonis@frontit.dk>
2023-12-25 12:41:38 +0200

    overallCapability to include cardio

--- src/stores/stats/physical.js
+++ src/stores/stats/physical.js
@@ -2,11 +2,14 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { storeName } from "@/stores";
 import { Balance } from "@/stats";
+import {useCardiovascular} from "@/stores/stats/cardiovascular.js";
 
 export const usePhysicalStore = defineStore(storeName('physical'), () => {
     const energy = Balance.create(-1000, 1000);
 
-    const overallCapability = computed(() => Balance.actualCenter(energy));
+    const cardiovascular = useCardiovascular();
+
+    const overallCapability = computed(() => Balance.actualCenter(energy) * cardiovascular.overallHealth);
 
     return {
         energy,
