53716340301e429f50bf6c4aef7b85daab88e578
mantas <mantas.pauliukonis@frontit.dk>
2023-12-26 09:30:49 +0200

    Cardio system damage prototype

--- src/stores/stats/physical.js
+++ src/stores/stats/physical.js
@@ -4,6 +4,7 @@ import { storeName } from "@/stores";
 import { Balance } from "@/stats";
 import {useCardiovascular} from "@/stores/stats/cardiovascular.js";
 import {computedOnce} from "@/stats/computed.js";
+import {onClock} from "@/routines/clock.js";
 
 export const usePhysicalStore = defineStore(storeName('physical'), () => {
     const energy = Balance.create(-1000, 1000);
@@ -11,6 +12,12 @@ export const usePhysicalStore = defineStore(storeName('physical'), () => {
     const cardiovascular = useCardiovascular();
 
     const overallCapability = computedOnce(() => Balance.actualCenter(energy) * cardiovascular.overallHealth);
+
+    onClock(() => {
+        const balance = (1 - Balance.actualCenter(energy)) * 5;
+
+        Balance.affect(cardiovascular.health, -balance);
+    });
 
     return {
         energy,
