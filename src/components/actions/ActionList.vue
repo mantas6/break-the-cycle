6ed66b94185536e486656c3f37918e2a8ba429c1
mantas <mantas.pauliukonis@frontit.dk>
2023-12-26 05:59:58 +0200

    Seprate cats in home view if more than one

--- src/components/actions/ActionList.vue
+++ src/components/actions/ActionList.vue
@@ -1,17 +1,28 @@
 <script setup>
 import { computed } from "vue";
 import ActionListItem from "./ActionListItem.vue";
+import {set, transform} from "lodash/object.js";
 
 const props = defineProps({
   items: Object,
   grid: Boolean,
 })
+
+const categorized = computed(() => {
+  const iteratee = (acc, action, actionName) => set(acc, [action.category, actionName], action);
+  return transform(props.items, iteratee, {});
+});
 </script>
 
 <template>
-  <div :class="{ 'grid-cols-2': grid }" class="grid gap-1">
-    <template v-for="(action, actionName) in items" :key="actionName">
-      <ActionListItem :name="actionName" v-bind="action" />
+  <div class="grid gap-1">
+    <template v-for="(actionsItems, categoryName) in categorized" :key="categoryName">
+      <div class="text-xs text-zinc-300" v-if="Object.keys(categorized).length > 1">{{ categoryName }}</div>
+      <div :class="{ 'grid-cols-2': grid }" class="grid gap-1">
+        <template v-for="(action, actionName) in actionsItems" :key="actionName">
+          <ActionListItem :name="actionName" v-bind="action" />
+        </template>
+      </div>
     </template>
   </div>
 </template>
