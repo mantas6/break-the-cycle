1b171e6449a3267d80abfd78319bd72cc3cbb1b7
mantas <mantas.pauliukonis@frontit.dk>
2023-12-26 07:18:35 +0200

    Improve death screen

--- src/components/DeathScreen.vue
+++ src/components/DeathScreen.vue
@@ -5,8 +5,8 @@
 </script>
 
 <template>
-  <div>
-    <div class="text-lg">You're dead</div>
-    <div>{{ death.reason }}</div>
+  <div class="w-full text-center p-10">
+    <div class="text-2xl text-red-200">You're dead</div>
+    <div class="text-sm"><span class="text-zinc-300">Reason:</span> {{ death.reason }}</div>
   </div>
 </template>
