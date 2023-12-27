618a1b907156739acb517ed9d351707834bf5ed1
mantas <mantas.pauliukonis@frontit.dk>
2023-12-27 12:10:31 +0200

    Add tmp options menu to death screen

--- src/components/DeathScreen.vue
+++ src/components/DeathScreen.vue
@@ -1,5 +1,6 @@
 <script setup>
  import {useDeathStore} from "@/stores/death.js";
+ import SettingsView from "@/views/SettingsView.vue";
 
  const death = useDeathStore();
 </script>
@@ -8,5 +9,6 @@
   <div class="w-full text-center p-10">
     <div class="text-2xl text-red-200">You're dead</div>
     <div class="text-sm"><span class="text-zinc-300">Reason:</span> {{ death.reason || 'Unknown' }}</div>
+    <SettingsView class="mt-10" />
   </div>
 </template>
