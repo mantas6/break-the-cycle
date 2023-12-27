1e05ad15577fbb743f9f1e399fab123cf5259980
mantas <mantas.pauliukonis@frontit.dk>
2023-12-27 13:02:49 +0200

    Planner component rework, unit formatting

--- src/components/panels/PlannerPanel.vue
+++ src/components/panels/PlannerPanel.vue
@@ -5,18 +5,21 @@ import ActionList from "@/components/actions/ActionList.vue";
 import { useActionsStore } from "@/stores/actions";
 import StatValue from "@/components/StatValue.vue";
 import {useUnlockStore} from "@/stores/unlock";
+import {computed} from "vue";
 
 const actions = useActionsStore();
 const unlock = useUnlockStore();
+
+const fullyUtilized = computed(() => actions.currentDuration === actions.maxDuration)
 </script>
 
 <template>
-  <PanelBlock class="flex flex-col" :locked="!unlock.planner">
+  <PanelBlock class="flex flex-col gap-2" :locked="!unlock.planner">
     <div class="grid grid-cols-2 gap-3">
-      <StatValue title="Planned hours">
-        <span>{{ actions.currentDuration }}h / {{ actions.maxDuration }}h</span>
-      </StatValue>
+      <StatValue title="Planned" format="hour" class="text-sm" :now="actions.currentDuration" />
+      <StatValue title="Idle" format="hour" class="text-sm" :class="{ 'text-red-300': !fullyUtilized }" :now="actions.maxDuration - actions.currentDuration" />
     </div>
     <ActionList :items="actions.allActive" />
+    <span v-if="!actions.currentDuration" class="text-xs text-center mt-10">No plans</span>
   </PanelBlock>
 </template>
--- src/directives/format.js
+++ src/directives/format.js
@@ -1,9 +1,20 @@
 export const vFormat = (el, { arg: style, value }) => {
+    const abstractStyles = ['decimal', 'currency', 'percent', undefined];
+
+    const usingAbstract = abstractStyles.includes(style);
+
     const options = {
-        style,
-        currency: style === 'currency' ? 'USD' : undefined,
+        style: usingAbstract ? style : 'unit',
         notation: value > 100000 ? 'compact' : 'standard',
     };
+
+    if (style === 'currency') {
+        options.currency = 'USD';
+    }
+
+    if (!usingAbstract) {
+        options.unit = style;
+    }
 
     const formatter = Intl.NumberFormat('en-US', options);
 
