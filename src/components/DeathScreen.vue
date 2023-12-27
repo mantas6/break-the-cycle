258ba8dc2c3e2d789e89fe051256cc9034e4c7dd
mantas <mantas.pauliukonis@frontit.dk>
2023-12-27 12:09:04 +0200

    Add unknown reason if empty

--- src/components/DeathScreen.vue
+++ src/components/DeathScreen.vue
@@ -7,6 +7,6 @@
 <template>
   <div class="w-full text-center p-10">
     <div class="text-2xl text-red-200">You're dead</div>
-    <div class="text-sm"><span class="text-zinc-300">Reason:</span> {{ death.reason }}</div>
+    <div class="text-sm"><span class="text-zinc-300">Reason:</span> {{ death.reason || 'Unknown' }}</div>
   </div>
 </template>
