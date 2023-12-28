e14c2e0c701fcd37f8f2dc04d8dab9d3e8bbf9f2
mantas <mantas.pauliukonis@frontit.dk>
2023-12-28 14:23:39 +0200

    Balancing

--- src/stores/stats/physical.js
+++ src/stores/stats/physical.js
@@ -7,14 +7,14 @@ import {computedOnce} from "@/helpers/computed.js";
 import {onClock} from "@/routines/clock.js";
 
 export const usePhysicalStore = defineStore(storeName('physical'), () => {
-    const energy = Balance.create(-1000, 1000);
+    const energy = Balance.create(-100, 100);
 
     const cardiovascular = useCardiovascular();
 
     const overallCapability = computedOnce(() => Balance.actualCenter(energy) * cardiovascular.overallHealth);
 
     onClock(() => {
-        const balance = (1 - Balance.actualCenter(energy)) * 5;
+        const balance = (1 - Balance.actualCenter(energy));
 
         Balance.affect(cardiovascular.health, -balance);
     });
