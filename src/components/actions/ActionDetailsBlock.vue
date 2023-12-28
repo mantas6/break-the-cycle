798d4edde0059362349bca5d57e141e64643c783
mantas <mantas.pauliukonis@frontit.dk>
2023-12-28 11:25:57 +0200

    Details component improvements

--- src/components/actions/ActionDetailsBlock.vue
+++ src/components/actions/ActionDetailsBlock.vue
@@ -3,6 +3,8 @@ import { ClockIcon } from '@heroicons/vue/16/solid'
 import {computed} from "vue";
 import {useUnlockStore} from "@/stores/unlock.js";
 
+defineEmits(['hover'])
+
 const props = defineProps({
   visible: Boolean,
   description: String,
@@ -26,9 +28,11 @@ const durationsHuman = computed(() => {
 
 <template>
   <template v-if="visible">
-    <div class="absolute z-10 top-full left-0 bg-zinc-500 border-dotted border border-zinc-400 p-3 text-xs">
-      <div>{{ description }}</div>
-      <div v-if="unlock.planner" class="flex gap-1"><ClockIcon class="w-4"/> {{ durationsHuman }}</div>
+    <div class="absolute z-10 top-full left-0 bg-zinc-500 border-dotted border border-zinc-400 p-3 text-xs" @mouseenter="$emit('hover')">
+      <div class="flex flex-col gap-1">
+        <div>{{ description }}</div>
+        <div v-if="unlock.planner" class="flex gap-1"><ClockIcon class="w-4"/> {{ durationsHuman }}</div>
+      </div>
     </div>
   </template>
 </template>
--- src/components/actions/ActionListItem.vue
+++ src/components/actions/ActionListItem.vue
@@ -78,6 +78,6 @@
       <button @click="actions.decrease(name)" :disabled="!isActive" v-hover="!isActive"><MinusCircleIcon class="w-7" /></button>
       <button @click="actions.remove(name)" :disabled="!isActive" v-hover="!isActive"><BackspaceIcon class="w-7" /></button>
     </div>
-    <ActionDetailsBlock :visible="showDetails && unlock.categories" v-bind="item" />
+    <ActionDetailsBlock :visible="showDetails && unlock.categories" v-bind="item" @hover="showDetails = false" />
   </div>
 </template>
