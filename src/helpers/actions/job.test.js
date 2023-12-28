47d5c89610e363a50bd6592c22846f4a69d06248
mantas <mantas.pauliukonis@frontit.dk>
2023-12-28 06:40:12 +0200

    Chargeable test

--- src/helpers/actions/job.test.js
+++ src/helpers/actions/job.test.js
@@ -4,5 +4,23 @@ import {createChargeable} from "@/helpers/actions/job.js";
 it('correctly calculates chargeable', () => {
     const { charge, onCharge, onFull } = createChargeable({ max: 10 });
 
-    //
+    expect(charge.now).toBe(0)
+    expect(charge.max).toBe(10)
+
+    let isFull = false;
+
+    onCharge(() => 1)
+    onFull(() => isFull = true)
+    expect(charge.now).toBe(1)
+    expect(isFull).toBe(false)
+
+    onCharge(() => 8)
+    onFull(() => isFull = true)
+    expect(charge.now).toBe(9)
+    expect(isFull).toBe(false)
+
+    onCharge(() => 1)
+    onFull(() => isFull = true)
+    expect(charge.now).toBe(0)
+    expect(isFull).toBe(true)
 })
