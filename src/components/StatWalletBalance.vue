a1afc2ca2b2c4da5e5b5df3a62fd07671a4745d1
mantas <mantas.pauliukonis@frontit.dk>
2023-12-31 10:39:50 +0200

    Use 16 icons

--- src/components/StatWalletBalance.vue
+++ src/components/StatWalletBalance.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/24/outline'
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/16/solid'
 
 const props = defineProps({
   now: Number,
