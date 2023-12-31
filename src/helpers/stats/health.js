655eb8b89f38d860aee289281ea1759542aa2df3
mantas <mantas.pauliukonis@frontit.dk>
2023-12-31 09:36:37 +0200

    Basic jsdoc block

--- src/helpers/stats/health.js
+++ src/helpers/stats/health.js
@@ -1,6 +1,18 @@
 import {Balance} from "@/stats/index.js";
 import {useDeathStore} from "@/stores/death.js";
 
+/**
+ * @typedef {Object} DegradeLifetimeOptions
+ * @property {number} passiveHealthLoss
+ * @property {number} healthLossMultiplier
+ */
+
+/**
+ *
+ * @param health
+ * @param healthLifetime
+ * @param {DegradeLifetimeOptions} opts
+ */
 export function degradeLifetime({ health, healthLifetime }, opts) {
     const passiveHealthLoss = opts.passiveHealthLoss || 0.01
     const healthLossMultiplier = opts.healthLossMultiplier || 10;
