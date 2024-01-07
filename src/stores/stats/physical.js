36ae097e292c4526d38a7ccb13b038126dbc7485
mantas <mantas.pauliukonis@frontit.dk>
2024-01-07 08:29:14 +0200

    Cleanup

--- src/stores/stats/physical.js
+++ src/stores/stats/physical.js
@@ -5,16 +5,32 @@ import {useCardiovascularStore} from "@/stores/stats/cardiovascular.js";
 import {computedOnce} from "@/helpers/computed.js";
 import {onClock} from "@/routines/clock.js";
 import {useRespiratoryStore} from "@/stores/stats/respiratory.js";
+import {computed} from "vue";
 
 export const usePhysicalStore = defineStore(storeName('physical'), () => {
-    const energy = Balance.create({ min: -100, max: 100, now: 50, lowerLimit: -99.9, upperLimit: 99.9 });
-
-    const muscleMass = Balance.create({ min: 0, max: 1000, now: 1, lowerLimit: 1 });
-
     const cardiovascular = useCardiovascularStore();
     const respiratory = useRespiratoryStore();
 
-    const overallCapability = computedOnce(() => Balance.actualCenter(energy, 0.25) * cardiovascular.overallHealth * respiratory.overallHealth);
+    const energy = Balance.create({
+        min: -100,
+        max: 100,
+        now: 50,
+        lowerLimit: -99.9,
+        upperLimit: 99.9,
+    });
+
+    const muscleMass = Balance.create({
+        min: 0,
+        max: 1000,
+        now: 1,
+        lowerLimit: 1,
+    });
+
+    // Still have to decide what will determine this stat
+    // Also the UI bar should display this as a green bg
+    const physicalEndurance = computed(() => 0.25);
+
+    const overallCapability = computedOnce(() => Balance.actualCenter(energy, physicalEndurance.value) * cardiovascular.overallHealth * respiratory.overallHealth);
 
     onClock(() => {
         const balance = (1 - Balance.actualCenter(energy, 0.25));
@@ -26,6 +42,8 @@ export const usePhysicalStore = defineStore(storeName('physical'), () => {
     return {
         energy,
         muscleMass,
+
+        physicalEndurance,
 
         overallCapability,
     };
