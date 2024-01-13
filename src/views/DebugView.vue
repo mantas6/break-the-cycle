1b3bfcafa2067a72c580c2f7f947d0100ab87fc3
mantas <mantas.pauliukonis@frontit.dk>
2024-01-13 11:22:34 +0200

    Debug: Balance increase shortcut

--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -3,15 +3,18 @@ import {useTimeStore} from "@/stores/time";
 import {ChevronDoubleLeftIcon, ChevronDoubleRightIcon} from "@heroicons/vue/24/outline";
 import {useUnlockStore} from "@/stores/unlock.js";
 import {useDebugStore} from "@/stores/debug.js";
+import {useWalletStore} from "@/stores/stats/wallet.js";
 
 const time = useTimeStore();
 const debug = useDebugStore();
+const wallet = useWalletStore();
 </script>
 
 <template>
   <div class="grid gap-3 text-center text-zinc-100 *:bg-zinc-700 hover:*:bg-zinc-800 active:*:bg-zinc-900 *:p-2 *:rounded">
     <button @click="debug.unlockAll" v-hover>Unlock All</button>
     <button @click="debug.unlockAllActions = true" v-hover>Unlock All Actions</button>
+    <button @click="wallet.transaction(1000)" v-hover>+1000$</button>
     <div class="flex gap-2 justify-center">
       <button @click="time.clockInterval += 50" v-hover><ChevronDoubleLeftIcon class="w-6" /></button>
       <span>{{ time.clockInterval }}ms</span>
