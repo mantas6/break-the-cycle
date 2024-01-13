cf47d42585d629e232bdd2feb25fc23d37cd104d
mantas <mantas.pauliukonis@frontit.dk>
2024-01-13 07:48:42 +0200

    Planner and hold automatic unlock depending on the intellect

--- src/stores/unlock.js
+++ src/stores/unlock.js
@@ -7,19 +7,21 @@ import {condition} from "@/helpers/state.js";
 import {useNutritionStore} from "@/stores/stats/nutrition.js";
 import {onClock} from "@/routines/clock.js";
 import {useActionsStore} from "@/stores/actions.js";
+import {useIntellectStore} from "@/stores/stats/intellect.js";
 
 export const useUnlockStore = defineStore(storeName('unlock'), () => {
     const wallet = useWalletStore();
     const actionsStore = useActionsStore();
     const physicalStore = usePhysicalStore();
     const nutritionStore = useNutritionStore();
+    const intellect = useIntellectStore();
 
     const balance = condition(() => wallet.balance.now > 0);
 
     const categories = condition(() => Object.keys(actionsStore.all).length > 1);
 
-    const planner = condition(() => false);
-    const hold = condition(() => false);
+    const planner = condition(() => intellect.overall > 10);
+    const hold = condition(() => intellect.overall > 5);
 
     const physical = condition(() => Balance.actualCenter(physicalStore.energy) < 0.5);
 
